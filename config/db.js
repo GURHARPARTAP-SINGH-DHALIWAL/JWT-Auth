1393fa682d37ba6bff7d69112c747ea841532c4f
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-09-16 11:24:39 +0530

    Changes

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 
 const connectDB=async ()=>{
     try{
-       const conn= await mongoose.connect("mongodb+srv://gurharpartap:gurharpartap@cluster0.u6chi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+       const conn= await mongoose.connect(process.env.MONGO_URI,{
         //    options to remove some of the warnings
         useNewUrlParser: true,
         useUnifiedTopology: true,
