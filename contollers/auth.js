db67110b7926636e076669065707a31ff6b41b0c
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-09-16 10:58:49 +0530

    Add Logout

--- contollers/auth.js
+++ contollers/auth.js
@@ -80,3 +80,9 @@ module.exports.login_verify=async (req,res) => {
       return res.status(400).json({errors});
     }
 };
+
+module.exports.logout=(req,res)=>{
+  res.cookie('jwt','',{maxAge:1});
+
+  return res.redirect('/login');
+};
--- routes/auth.js
+++ routes/auth.js
@@ -16,6 +16,8 @@ router.get('/signup',isGuest,authController.sign_up_page);
 router.post('/signup',authController.sign_up_add);
 router.get('/login',isGuest,authController.login_up_page);
 router.post('/login',authController.login_verify);
+router.get('/logout',isAuth,authController.logout);
+
 
 
 
