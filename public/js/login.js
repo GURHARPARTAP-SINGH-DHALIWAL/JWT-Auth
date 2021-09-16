bfd75114809598b2fbfd279ff2466edcde01584d
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-09-16 09:47:10 +0530

    Design forms

---
+++ public/js/login.js
@@ -0,0 +1,46 @@
+
+window.addEventListener('load',function(){
+
+    const form=document.getElementById('login-form');
+    const emailError=document.getElementById("errE");
+    const passwordError=document.getElementById("errP");
+    console.log("hello");
+     console.log("hello");
+    console.log(form);
+      form.addEventListener("submit",async (e)=>{
+          e.preventDefault();
+          emailError.textContent='';
+          passwordError.textContent='';
+          const name=form.name.value;
+          const email=form.email.value;
+          const password=form.password.value;
+          try{
+          const res=await fetch('/login',{
+              method:'POST',
+              body:JSON.stringify({email,password}),
+              headers:{'Content-Type':'application/json'}
+    
+          });
+          const data=await res.json();
+          if(data.errors)
+          {
+              emailError.textContent=data.errors.email;
+              passwordError.textContent=data.errors.password;
+          }
+
+          if(data.user)
+          {
+              location.assign('/');
+          }
+        }
+        catch(err)
+        {
+            console.log(err);
+    
+        }
+    
+          console.log(name," ",email," ",password);
+      });
+    
+    
+    });
--- public/js/signup.js
+++ public/js/signup.js
@@ -2,11 +2,15 @@
 window.addEventListener('load',function(){
 
 const form=document.getElementById('signup-form');
+const emailError=document.getElementById("errE");
+const passwordError=document.getElementById("errP");
 console.log("hello");
  console.log("hello");
 console.log(form);
   form.addEventListener("submit",async (e)=>{
       e.preventDefault();
+      emailError.textContent='';
+      passwordError.textContent='';
       const name=form.name.value;
       const email=form.email.value;
       const password=form.password.value;
@@ -17,11 +21,24 @@ console.log(form);
           headers:{'Content-Type':'application/json'}
 
       });
+      const data=await res.json();
+      if(data.errors)
+      {
+          emailError.textContent=data.errors.email;
+          passwordError.textContent=data.errors.password;
+      }
+
+      if(data.user)
+      {
+          location.assign('/');
+      }
+
+      
     }
     catch(err)
     {
         console.log(err);
-            
+
     }
 
       console.log(name," ",email," ",password);
